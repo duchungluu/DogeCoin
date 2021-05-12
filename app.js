45e558dc00c0165d71d1403014569167ccf5f1dc
Hung Luu <duchung.luu@live.com>
2021-05-12 16:11:18 +0300

    Add sql server

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-
+const sql = require('mssql');
 
 const app = express();
 const port= process.env.PORT || 3000;
 
+const config = {
+  user: 'test',
+  password: 'Est12345!',
+  server: 'duclu.database.windows.net',
+  database: 'PSLibary',
 
-app.use(morgan('combined'));
+  options: {
+    encrypt:true
+  }
+};
+//sql.connect(config).catch(err => debug(err));
+
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -20,12 +31,14 @@ app.set('view engine', 'ejs');
 
 const nav = [
   { link: '/coins', title: 'Coin'},
-  {link: '/platforms', title: 'Platform'}
+  { link: '/platforms', title: 'Platform'}
 ];
 
 const coinRouter = require('./src/routes/coinRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use('/coins', coinRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', (req, res) => {
   res.render(
--- src/routes/coinRoutes.js
+++ src/routes/coinRoutes.js
@@ -1,47 +1,75 @@
 const express = require('express');
+const { MongoClient, ObjectID } = require('mongodb');
+const debug = require('debug')('app:coinRoutes');
+
 const coinRouter = express.Router();
 
-function router(nav){
-  const coins = [
-    {
-      title: 'Bitoin',
-      genre: 'crypto',
-      author: 'Satoshi',
-      read: false
-    },
-    {
-      title: 'LuuCoin',
-      genre: 'cryptonite',
-      author: 'Luu',
-      read: true
-    },
-  ]
+function router(nav) {
   coinRouter.route('/')
-    .get((req,res) => {
-      res.render(
-        'coinListView',
-        {
-          nav, 
-          title: 'To The MOON' ,
-          coins
+    .get((req, res) => {
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('coins');
+
+          const coins = await col.find().toArray();
+
+          res.render(
+            'coinListView',
+            {
+              nav,
+              title: 'Library',
+              coins
+            }
+          );
+        } catch (err) {
+          debug(err.stack);
         }
-        );
+        client.close();
+      }());
     });
-  
+
   coinRouter.route('/:id')
-    .get((req,res) => {
- //  const id = req.params.id;  SAME AS BELOW
-     const { id } = req.params;
-      res.render(
-        'coinView',
-        {
-          nav, 
-          title: 'To The MOON' ,
-          coin: coins[id]
+    .get((req, res) => {
+      const { id } = req.params;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('coins');
+
+          const coin = await col.findOne({ _id: new ObjectID(id) });
+          debug(coin);
+          res.render(
+            'coinView',
+            {
+              nav,
+              title: 'Library',
+              coin
+            }
+          );
+        } catch (err) {
+          debug(err.stack);
         }
-        );
+      }());
     });
-    return coinRouter
+  return coinRouter;
 }
 
-    module.exports = router;
+
+module.exports = router;
