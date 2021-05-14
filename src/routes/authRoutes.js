0ba0ebfd6c6b80a66698bd292fe4489b01214e42
Hung Luu <duchung.luu@live.com>
2021-05-14 15:15:25 +0300

    Add auth

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const {MongoClient} = require('mongodb');
+const debug = require('debug')('app:authRoutes');
+
+const authRouter = express.Router();
+
+function router() {
+    authRouter.route('/signUp')
+        .post((req, res) => )
+}
